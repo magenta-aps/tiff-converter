c7a7e2cd8d3ed20b071a1ae1b22e34f864f098ce
Andreas Kring <andreas@magenta.dk>
2017-09-12 06:50:50 -0700

    Added more command line tweeks; added clean script

--- populate_docs.py
+++ populate_docs.py
@@ -3,15 +3,17 @@ import shutil
 import sys
 
 TEMPLATE_DOC = 'demo.docx'
-TARGET_DIR = 'C:/Users/IEUser/Documents/jscript/docs2'
-NUMBER_OF_COPIES = 100
+# TARGET_DIR = 'C:/Users/IEUser/Documents/jscript/docs2'
+NUMBER_OF_COPIES = 500
 
-if len(sys.argv) > 1:
-    offset = int(sys.argv[1])
+doc_dir = sys.argv[1]
+
+if len(sys.argv) > 2:
+    offset = int(sys.argv[2])
 else:
     offset = 0
 
-target_dir = os.path.abspath(TARGET_DIR)
+target_dir = os.path.abspath(doc_dir)
 if not os.path.isdir(target_dir):
     os.mkdir(target_dir)
 
@@ -20,5 +22,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 for i in range(NUMBER_OF_COPIES):
     filename = name + str(i + offset) + ext
     src = os.path.join(basedir, TEMPLATE_DOC)
-    dst = os.path.join(TARGET_DIR, filename)
+    dst = os.path.join(target_dir, filename)
     shutil.copyfile(src, dst)
