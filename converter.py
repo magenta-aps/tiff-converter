6e1c17cebc1a5ebd0a9c2872afc9de9df01ba018
Andreas Kring <andreas@magenta.dk>
2017-09-12 02:20:39 -0700

    Initial commit

---
+++ converter.py
@@ -0,0 +1,20 @@
+import os
+import comtypes.client
+
+WD_FORMAT_PDF = 17
+SOURCE_DIR = 'C:/Users/IEUser/Documents/jscript/docs'
+TARGET_DIR = 'C:/Users/IEUser/Documents/jscript/pdfs'
+
+docs = [os.path.join(os.path.abspath(SOURCE_DIR), f) for f in os.listdir(SOURCE_DIR)]
+pdfs = [os.path.join(os.path.abspath(TARGET_DIR), os.path.splitext(f)[0] + '.pdf') for f in os.listdir(SOURCE_DIR)]
+
+word_app = comtypes.client.CreateObject('Word.Application')
+word_app.Visible = False
+
+for i in range(len(docs)):
+    print(docs[i])
+    doc = word_app.Documents.Open(docs[i])
+    doc.SaveAs(pdfs[i], FileFormat=WD_FORMAT_PDF)
+    doc.Close()
+
+word_app.Quit()
