67e88ede5a622d213cbef9fb9da837a3623b5e8f
Andreas Kring <andreas@magenta.dk>
2017-09-20 01:28:48 -0700

    Handles both tiff conversions, i.e. the tiff from the pdf-to-tiff conversion is now also compressed using LZW

--- tiff/tiffconverter.py
+++ tiff/tiffconverter.py
@@ -1,6 +1,7 @@
 import os
 import platform
 import subprocess
+import tempfile
 
 GS_ARGS = [
     'gswin32c' if platform.system() == 'Windows' else 'gs',
@@ -13,13 +14,17 @@ GS_ARGS = [
 ]
 
 COMMAND = ' '.join(GS_ARGS)
+TEMP_DIR = tempfile.gettempdir()
 
 
 def convert(pdf: os.path.abspath, tiff: os.path.abspath) -> bool:
-    shell_command = COMMAND + ' -sOutputFile=' + tiff + ' -f ' + pdf
+    temp_tiff = os.path.join(TEMP_DIR, os.path.basename(tiff))
+    shell_command = COMMAND + ' -sOutputFile=' + temp_tiff + ' -f ' + pdf
     subprocess.call(shell_command, shell=True, stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE)
-    if os.path.isfile(tiff):
-        return True
-    else:
-        return False
+    if os.path.isfile(temp_tiff):
+        subprocess.call('convert ' + temp_tiff + ' -compress lzw ' + tiff,
+                        shell=True)
+        if os.path.isfile(tiff):
+            return True
+    return False
