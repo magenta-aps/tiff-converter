aad592f26539974d11dcbeaf3ce18a17001207c3
Andreas Kring <andreas@magenta.dk>
2017-09-27 02:55:38 -0700

    Sandbox Excel spreadsheet converter added

---
+++ sandbox/converter_excel.py
@@ -0,0 +1,21 @@
+import os
+import comtypes.client
+
+FORMAT_PDF = 17
+SOURCE_DIR = 'C:/Users/IEUser/Documents/jscript/test/resources/root3'
+TARGET_DIR = 'C:/Users/IEUser/Documents/jscript'
+
+app = comtypes.client.CreateObject('Excel.Application')
+app.Visible = False
+
+infile = os.path.join(os.path.abspath(SOURCE_DIR), 'spreadsheet1.xlsx')
+outfile = os.path.join(os.path.abspath(TARGET_DIR), 'spreadsheet1.pdf')
+
+print(infile)
+print(outfile)
+
+doc = app.Workbooks.Open(infile)
+doc.SaveAs(outfile, FileFormat=FORMAT_PDF)
+doc.Close()
+
+app.Quit()
